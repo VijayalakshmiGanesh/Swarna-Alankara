e2b1e64e0f291b9d9773c1b54e994be0b35a1a99
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-05-16 14:09:22 +0530

    Backend data and other dependency added

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+   content: [
+     "./src/**/*.{js,jsx,ts,tsx}",
+   ],
+   theme: {
+    // colors: {
+    //   'icon-yellow': '#efa939',
+
+    // },
+     extend: {},
+   },
+   plugins: [],
+  }
