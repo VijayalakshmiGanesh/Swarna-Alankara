acd75521cf7f0931db31c8c4cd37118b654b7392
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-05-17 22:45:21 +0530

    Initial, basic details displayed in Productdetail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,25 @@
+import { useContext } from "react"
+import { useParams } from "react-router-dom"
+import ProductContext from "../contexts/ProductContext"
+
+
 function ProductDetail(){
+const {id} = useParams()
+const {productList} = useContext(ProductContext);
+
+const findProduct = productList?.find(({_id}) => _id === id);
+const { title, imageURL, category, subCategory, weight} = findProduct;
+
     return (
-        <><h1>ProductDetail</h1></>
+        <><h1>ProductDetail: {id}</h1>
+        <div>
+            <img src={imageURL} alt="picture title" height="300" width="350"/>
+            <h2>{title}</h2>
+            <p>Category: {category}</p>
+            <p>Sub-category: {subCategory}</p>
+            <p>Weight: {weight}</p>
+        </div>
+        </>
     )
 }
 
