49439b4c6a76d477e2bf2d0b35f4210c5661b4cd
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-05-20 22:12:48 +0530

    cart context functionality done

--- src/contexts/CartContext.jsx
+++ src/contexts/CartContext.jsx
@@ -1,20 +1,176 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const CartContext = createContext();
 
 export function CartProvider({ children }) {
 
-    const AddToCart = async (productToAddInCart) => {
-        try { 
-            const response = await fetch("/api/user/cart");
-            
+    const [itemsInCart, setItemsInCart] = useState([]);
 
+    const getCartItems = async () => {
+        try { 
+            const response = await fetch("/api/user/cart", {
+                method: "GET",
+                headers: {
+                    "authorization": localStorage.getItem("key"),
+                },
+             });
+            
+             if (response.status === 200) {
+                 setItemsInCart(JSON.parse(response._bodyInit).cart)
+            }
         } catch (e) {
             console.log(e)
-        }
+        } 
     }
 
-    return (
-        <CartContext.Provider value={{}}>{ children}</CartContext.Provider>
-    )
+    const isItemInCart = (idToFind) => itemsInCart.findIndex(({ _id }) => _id === idToFind)
+    
+    const addItemToCart = async (productToAddInCart) => {
+        const isItemFound = isItemInCart(productToAddInCart._id);
+        if (isItemFound === -1) {
+            try {
+                const response = await fetch("/api/user/cart", {
+                    method: "POST",
+                    headers: {
+                        "authorization": localStorage.getItem("key"),
+                    },
+                    body: JSON.stringify({ product: productToAddInCart }),
+                });
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        else {
+                try { 
+                    const response = await fetch(`/api/user/cart/${productToAddInCart._id}`, {
+                        method: "POST",
+                        headers: {
+                            "authorization": localStorage.getItem("key"),
+                        },
+                        body: JSON.stringify({
+                            action: {
+                                type: "increment"
+                            }
+                        }),
+                    });
+                } catch (e) {
+                    console.log(e)
+                } 
+            } 
+        getCartItems();
+    }
+
+    const removeItemFromCart = async (productToBeRemovedFromCartID) => {
+        try {
+                const response = await fetch(`/api/user/cart/${productToBeRemovedFromCartID}`, {
+                    method: "DELETE",
+                    headers: {
+                        "authorization": localStorage.getItem("key"),
+                    },
+                   
+                });
+            } catch (e) {
+                console.log(e)
+        } 
+        getCartItems();
+    }
+
+    const reduceItemQuantity = async (itemToReduceQuantityID, qty) => {
+        if (qty > 1) {
+            try {
+                const response = await fetch(`/api/user/cart/${itemToReduceQuantityID}`, {
+                    method: "POST",
+                    headers: {
+                        "authorization": localStorage.getItem("key"),
+                    },
+                    body: JSON.stringify({
+                            action: {
+                                type: "decrement"
+                            }
+                        }),
+                   
+                });
+            } catch (e) {
+                console.log(e)
+            } 
+        } else {
+            removeItemFromCart(itemToReduceQuantityID)
+        }
+         
+        getCartItems();
+    }
+    useEffect(() => getCartItems, [])
+    return (<CartContext.Provider value={{itemsInCart, isItemInCart, getCartItems, addItemToCart, removeItemFromCart, reduceItemQuantity }}>{ children}</CartContext.Provider>)
 }
+
+// export function CartProvider({ children }) {
+
+//     const [messageFromAPI, setMessageFromAPI] = useState("")
+    
+
+    
+//     const CartOperations = (result, action) => {
+//         console.log(action)
+//         switch (action.type) {
+//             case "AddItemToCart":
+//                 console.log("add item to cart ", {...result, cart: [...result.cart, action.payload]})
+//                 return  {...result, cart: [...result.cart, action.payload]};
+        
+//             case "IncrementIteminCart": 
+//                 console.log("increment",result.cart.filter((product) => product._id === action.payload ?{ ...product, qty: product.qty + 1 } :product))
+//                 return {...result, cart: result.cart.filter((product) => product._id === action.payload ? { ...product, qty: product.qty + 1 } : product)};
+//             case "DecrementItemInCart":
+//                 return result.cart.filter((product) => product._id === action.payload ? { ...product, qty: product.qty - 1 } : product);
+//             case "RemoveItemFromCart":
+//                 return result.cart.filter(({ _id }) => _id !== action.payload)
+//             default:
+//                 return result;
+//         }
+//     }
+//     const [state, dispatch] = useReducer(CartOperations, {
+//         wishlist: [],
+//         cart: []
+//     })
+//     const findItem = (arr, itemToFind) => state[arr].findIndex(({_id}) => _id === itemToFind)
+//     console.log("state", state)
+        
+//     const AddToCart = async (productToAddInCart) => {
+//         const IsItemFound = findItem("cart", productToAddInCart._id)
+//         console.log("IsItemFound", IsItemFound)
+//         if (IsItemFound === -1) {
+//             try { 
+//             const response = await fetch("/api/user/cart", {
+//       method: "POST",
+//       headers: {
+//         "authorization": localStorage.getItem("key"),
+//       },
+//       body: JSON.stringify({product: productToAddInCart}),
+//     });
+            
+//             setMessageFromAPI(response)
+//             dispatch({type: "AddItemToCart", payload: {...productToAddInCart, qty: 1}})
+//         } catch (e) {
+//             console.log(e)
+//         } 
+//         } else {
+//              try { 
+//             const response = await fetch(`/api/user/cart/${productToAddInCart._id}`, {
+//       method: "POST",
+//       headers: {
+//         "authorization": localStorage.getItem("key"),
+//       },
+//                 body: JSON.stringify({
+//                     action: {
+//                         type: "increment"
+//                     }
+//                 }),
+//     });
+            
+//             setMessageFromAPI(response)
+//             dispatch({type: "IncrementIteminCart", payload: productToAddInCart._id})
+//         } catch (e) {
+//             console.log(e)
+//         } 
+            
+//         }
+//     }
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,74 +1,135 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { CartContext } from "../contexts/CartContext"
+import { NavLink } from "react-router-dom";
 
 function Cart() {
-    const { getCartItems } = useContext(CartContext);
-const [cartList, setCartList] = useState()
-
-    // const getCartItems = async () => {
-    //      try { 
-    //         const response = await fetch("/api/user/cart", {
-    //   method: "GET",
-    //   headers: {
-    //     "authorization": localStorage.getItem("key"),
-    //   },
-      
-    // });
-            
-    //          if (response.status === 200) {
-    //              setCartList(JSON.parse(response._bodyInit).cart)
-    //              console.log(cartList)
-    //         }
-    //     } catch (e) {
-    //         console.log(e)
-    //     } 
-    // }
-    useEffect(() =>{ setCartList(getCartItems())}, [])
-
-    const deleteAnCartItem = async (id) => {
-        try { 
-            const response = await fetch(`/api/user/cart/${id}`, {
-      method: "POST",
-      headers: {
-        "authorization": localStorage.getItem("key"),
-                },
-      body: JSON.stringify({action: {
-    type: "decrement"
-  }}),
-      
-    });
-            console.log("delete",JSON.parse(response._bodyInit).cart)
-             if (response.status === 200) {
-                 setCartList(JSON.parse(response._bodyInit).cart)
-                 console.log(cartList)
-            }
-        } catch (e) {
-            console.log(e)
-        } 
-    }
+    const { itemsInCart, getCartItems, addItemToCart, removeItemFromCart, reduceItemQuantity } = useContext(CartContext);
+    
+    useEffect(() => {
+        getCartItems()
+    }, [])
     return (
-        <><h1>Cart</h1>
+        <div>
+            {/* <div className="relative h-[250px] bg-neutral-300">
+                <img src="https://images.pexels.com/photos/9953655/pexels-photo-9953655.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="Image" class="w-full object-cover h-full blur-sm" />
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <h1 className="text-4xl font-bold text-[#efa939]">Shopping cart</h1>
+                </div>
+            </div> */}
             {
-                cartList?.length === 0 ? (<p>No items in the cart</p>) : (
-                    <> 
-                        {
-                            cartList?.map((product) => {
-                                 const {_id, title, qty, price, id} = product
-                            return(
-                                <div key={_id} className="bg-cyan-500 shadow-lg shadow-cyan-500/50 m-3">
-                                    <p>{title}</p>
-                                    <p>{qty}</p>
-                                    <p>{price}</p>
-                                    <button className="bg-blue-700 p-3 rounded text-white" onClick={() => deleteAnCartItem(_id)}>Delete</button>
-                                </div>
-                            )
-                        })
-                       }
-                    </>
-                )
-        }
-        </>
+                itemsInCart?.length === 0 ?
+                    (<div className="flex justify-center items-center py-2 my-2 md:my-5 md:py-5 flex-col md:flex-row">
+                        <img src="../assests/emptyCart.svg" height="500" width="500" />
+                        <div className="flex flex-col">
+                            <p className="text-2xl font-bold my-5">No items in cart</p>
+                            <p>
+                                <NavLink to="/products" className="bg-pink-700 rounded-lg my-3 px-2 py-3">Go to Products &gt;</NavLink>
+                            </p>
+                        </div>
+                    </div>) : (
+                        <div className="container mx-auto xl:py-7  xl:my-7 flex ">
+                <div className="order-items flex flex-col text-left w-[3/5]">
+                    <div className="flex justify-around text-blue-950 font-bold text-lg border-b-2">
+                        <span className="grow">Product</span>
+                        <span className="w-1/5 text-blue-950 ">Price</span>
+                        <span className="w-1/5">Quantity</span>
+                        <span className="w-1/5 text-blue-950"> Total</span>
+                        <span>&nbsp; &nbsp; &nbsp;</span>
+                    </div>
+                    {itemsInCart?.length === 0 ? (<p>No items in cart</p>) : (
+                        <>
+                            {
+                                 itemsInCart.map((item) => {
+                                const { title, qty, subCategory, price, weight, id, _id, imageURL } = item;
+
+                                return (
+                                    <div className="flex justify-around border-b-2 items-center" key={id}>
+                        
+                                        <div className="grow">
+                                            <NavLink to={`/product-detail/${_id}`} className=" flex items-center">
+                            <img src={imageURL} alt={`${title} thumbnail`} className="h-[100px] w-[100px]"/>
+                            <div className="flex flex-col px-2"> 
+                                <p>{title}</p>
+                                                <p>{subCategory}</p>
+                                                    <p>{weight}</p>
+                                                    
+                                                </div>
+                                                </NavLink>
+                        </div>
+                                        <span className="w-1/5">{ price}</span>
+                        <span className="w-1/5">
+                            <button className="px-1 bg-blue-900 text-white py-1 px-2 rounded-lg mx-2 min-w-[25px] font-bold" onClick={() => addItemToCart(item)}>+</button>
+                            <span>{qty}</span>
+                            <button className="px-1 bg-blue-900 text-white py-1 px-2 rounded-lg mx-2 min-w-[25px] font-bold" onClick={() => reduceItemQuantity(item._id, qty) }>-</button>
+                        </span>
+                                        <span className="w-1/5">Rs. {qty * price}</span>
+                                        <span className="w-1/5 flex flex-col"><button className="rounded-full bg-red-700 text-white hover:bg-white hover:text-red-700 m-3 px-2 py-1" onClick={() => removeItemFromCart(item._id)}>Delete</button>
+                                        <button className="rounded-full bg-red-700 text-white hover:bg-white hover:text-red-700 px-2 py-1">Move to wishlist</button></span>
+                    </div>
+                                )
+                            })
+                            }
+                           
+                        </>
+                    )
+                    }
+                </div>
+                {/* <div className="cart-total w-[2/5]">
+                    <h2>Cart total</h2>
+                    <div className="  bg-neutral-100">
+                        <div className="flex border-b-2">
+                            <p>Have a coupon? </p>
+                            <button>Apply</button>
+                        </div>
+                        
+                            <p className="border-b-2">Price details</p>
+                        <div className="border-b-2">
+                            <p>
+                            <span>Price: [items]</span>
+                            <span>₹ 1200</span>
+                        </p>
+                        <p>
+                            <span>Discount:</span>
+                            <span>₹ 1200</span>
+                        </p>
+                        <p>
+                            <span>Delivery Charges</span>
+                            <span>₹ 1200</span>
+                        </p>
+                        </div>
+                        <p>Total Amount: ....</p>
+                    </div>
+                    <button>CHECK OUT</button>
+                </div> */}
+            </div>
+                    )
+            }
+        </div>
     )
+    
+    // return (
+    //     <><h1>Cart</h1>
+    //         {
+    //             cartList?.length === 0 ? (<p>No items in the cart</p>) : (
+    //                 <> 
+    //                     {
+    //                         cartList?.map((product) => {
+    //                              const {_id, title, qty, price, id} = product
+    //                         return(
+    //                             <div key={_id} className="bg-cyan-500 shadow-lg shadow-cyan-500/50 m-3">
+    //                                 <p>{title}</p>
+    //                                 <p>{qty}</p>
+    //                                 <p>{price}</p>
+    //                                 <button className="bg-blue-700 p-3 rounded text-white" onClick={() => deleteAnCartItem(_id)}>Delete</button>
+    //                             </div>
+    //                         )
+    //                     })
+    //                    }
+    //                 </>
+    //             )
+    //     }
+    //     </>
+    // )
 }
 
 export default Cart
