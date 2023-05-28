20a7a23331045e21a64b0e7c5bdd5a8321989a65
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-05-28 21:41:06 +0530

    created a initial layout for user profile page

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+// import { NavLink } from 'react-router-dom';
 
 function UserProfile() {
   const { loggedInUserDetails } = useContext(AuthContext);
@@ -99,72 +100,109 @@ function UserProfile() {
 
   return (
     <>
-      <div>
-        <h1>User Profile </h1>
-        <p>
-          Name: {loggedInUserDetails.firstName} {loggedInUserDetails.lastName}
-        </p>
-        <p>E-mail: {loggedInUserDetails.email}</p>
-        <button>Logout</button>
-      </div>
-      <div>
-        <h1>Address</h1>
-        {address?.length === 0 ? (
-          <p>No address available</p>
-        ) : (
-          address.map(i => {
-            const { _id, name, street, city, state, country, zipCode, mobile } =
-              i;
-            return (
-              <div key={_id}>
-                <p>
-                  {`${name}, ${street}, ${city}, ${state}, ${country} - ${zipCode}`}
-                </p>
-                <p>{`${mobile}`}</p>
-                <button
-                  className="rounded-full bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold  my-1 px-2 py-1"
-                  onClick={() => deleteAddressFromAddressBook(_id)}
-                >
-                  Delete
-                </button>
-                <button
-                  className="rounded-full bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold  my-1 px-2 py-1"
-                  onClick={() =>
-                    updateAddressInAddressBook(_id, {
-                      _id,
-                      name: 'Remo',
-                      street: '123, Dubai kuruku sandu',
-                      city: 'Dubai',
-                      state: 'Abu dhabi ',
-                      country: 'UAE',
-                      zipCode: '74AA14',
-                      mobile: '+133 - 98751200',
-                    })
-                  }
-                >
-                  Update
-                </button>
-                <hr />
-              </div>
-            );
-          })
-        )}
-        <button
-          className="rounded-full bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold  my-1 px-2 py-1"
-          onClick={() =>
-            addAddressToAddressBook({
-              name: 'Remo',
-              street: '123, ADB Street',
-              city: 'Dubai',
-              state: 'Abu dhabi ',
-              country: 'UAE',
-              zipCode: '74AA14',
-              mobile: '+12298751200',
-            })
-          }
-        >
-          Add address
-        </button>
+      <div className="relative flex justify-center items-center text-left ">
+        {/* <div className='w-[400px] h-[400px] p-5 bg-white shadow-lg h-full flex flex-col items-center border-1 border-solid border-slate-700'> */}
+        <div className=" my-16 py-5 px-7 drop-shadow-lg w-[370px] border-2 border-solid border-gray-50 shadow-lg">
+          {/* <p className="text-gray-400 text-xs text-center">
+            User account details
+          </p> */}
+          <div className="my-5 max-w-xs">
+            <div className="pb-2">
+              <p className="text-2xl font-semibold text-center underline decoration-1 underline-offset-4 pb-4">
+                User account details
+              </p>
+              <p className="my-1">
+                <span className="font-semibold">Username:</span>
+                <span className="text-slate-900 px-1 ">
+                  {loggedInUserDetails.firstName} {loggedInUserDetails.lastName}
+                </span>
+              </p>
+              <p className="my-1">
+                <span className="font-semibold">E-mail:</span>
+                <span className="text-slate-900 px-1 ">
+                  {loggedInUserDetails.email}
+                </span>
+              </p>
+            </div>
+            <div className="py-2">
+              <p className="text-2xl font-semibold text-center underline decoration-1 underline-offset-4 ">
+                Address details
+              </p>
+              {address?.length === 0 ? (
+                <p>No address available</p>
+              ) : (
+                address.map(i => {
+                  const {
+                    _id,
+                    name,
+                    street,
+                    city,
+                    state,
+                    country,
+                    zipCode,
+                    mobile,
+                  } = i;
+                  return (
+                    <div key={_id} className="mt-3">
+                      <p className="font-semibold">{name}</p>
+                      <p className="text-slate-700 text-sm ">
+                        {`${street}, ${city}, ${state}, ${country} `}
+                      </p>
+                      <p className="text-slate-700 text-sm ">{zipCode}</p>
+                      <p className="text-slate-700 text-sm ">{`Phone Number: ${mobile}`}</p>
+                      <p className="flex">
+                        <button
+                          className="rounded-full w-3/4 my-3 py-1 bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold  my-1 px-2 py-1"
+                          onClick={() => deleteAddressFromAddressBook(_id)}
+                        >
+                          Delete
+                        </button>
+                        <button
+                          className="rounded-full w-3/4 my-3 py-1 bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold mx-3  my-1 px-2 py-1"
+                          onClick={() =>
+                            updateAddressInAddressBook(_id, {
+                              _id,
+                              name: 'Remo',
+                              street: '123, Dubai kuruku sandu',
+                              city: 'Dubai',
+                              state: 'Abu dhabi ',
+                              country: 'UAE',
+                              zipCode: '74AA14',
+                              mobile: '+133 - 98751200',
+                            })
+                          }
+                        >
+                          Update
+                        </button>
+                      </p>
+                    </div>
+                  );
+                })
+              )}
+            </div>
+            <button
+              className="rounded-full w-full bg-pink-700 text-white hover:bg-white hover:text-pink-700 hover:border-pink-700 hover:border-solid hover:border-2 hover:font-semibold  my-1 px-2 py-1"
+              onClick={() =>
+                addAddressToAddressBook({
+                  name: 'Remo',
+                  street: '123, ADB Street',
+                  city: 'Dubai',
+                  state: 'Abu dhabi ',
+                  country: 'UAE',
+                  zipCode: '74AA14',
+                  mobile: '+12298751200',
+                })
+              }
+            >
+              Add new address
+            </button>
+            <div className="flex mb-5 ">
+              <button className="w-full text-white bg-red-700 mx-1 p-3 rounded-md font-bold">
+                Logout
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
