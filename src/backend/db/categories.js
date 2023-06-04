5126af74c8cb27bba429c56ae60b0d5a0292eea8
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-04 22:11:59 +0530

    added image URl to data

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,4 +1,4 @@
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 /**
  * Category Database can be added here.
@@ -8,15 +8,16 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Gold",
-    description:
-      "The ultimate investment in luxury and beauty",
+    categoryName: 'Gold',
+    description: 'The ultimate investment in luxury and beauty',
+    imageUrl:
+      'http://www.fashionteria.com/wp-content/uploads/2021/04/gold-jewelry.jpg',
   },
   {
     _id: uuid(),
-    categoryName: "Silver",
-    description:
-      "A precious metal that is both beautiful and affordable",
+    categoryName: 'Silver',
+    description: 'A precious metal that is both beautiful and affordable',
+    imageUrl:
+      'https://blog.brilliance.com/wp-content/uploads/2016/07/jewelery.jpg',
   },
-  
 ];
