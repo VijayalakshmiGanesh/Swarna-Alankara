3eaf6ffdc0ba9ffe833a8626e198781b8ccb4cda
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-04 22:10:30 +0530

    updated with new context and removed unwanted ones

--- src/index.js
+++ src/index.js
@@ -1,37 +1,31 @@
-import React from "react";
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import {BrowserRouter as Router} from "react-router-dom";
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import "./index.css";
-import App from "./App";
-import { makeServer } from "./server";
-import {ProductProvider} from "./contexts/ProductContext";
-import { AuthProvider } from "./contexts/AuthContext";
-import { CartProvider } from "./contexts/CartContext";
-import { WishListProvider } from "./contexts/WishListContext";
+import './index.css';
+import App from './App';
+import { makeServer } from './server';
+import { AuthProvider } from './contexts/AuthContext';
+import { AddressProvider } from './contexts/AddressContext';
+import { DataProvider } from './contexts/DataContext';
 
 // Call make Server
 makeServer();
 
-
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-
   <StrictMode>
     <Router>
-      <ProductProvider>
-        <AuthProvider>
-          <CartProvider>
-            <WishListProvider>
-               <App />
-           </WishListProvider>
-          </CartProvider>
-        </AuthProvider>
-      </ProductProvider>
+      <AuthProvider>
+        <AddressProvider>
+          <DataProvider>
+            <App />
+          </DataProvider>
+        </AddressProvider>
+      </AuthProvider>
     </Router>
   </StrictMode>
 );
