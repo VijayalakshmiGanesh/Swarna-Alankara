b778cda6fcc3b646d263ac3054dc086038df9563
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-07 09:27:25 +0530

    Added order related info to reducer

--- src/redux/Data.jsx
+++ src/redux/Data.jsx
@@ -1,11 +1,7 @@
 export const dataReducer = (result, action) => {
-  console.log('action value in reducer', action);
+  // console.log('action value in reducer', action);
   switch (action.type) {
     case 'CategoryList':
-      console.log('action value in categoryList', {
-        ...result,
-        categoryList: action.payload,
-      });
       return { ...result, categoryList: action.payload };
 
     case 'ProductList':
@@ -26,6 +22,21 @@ export const dataReducer = (result, action) => {
     case 'setLoading':
       return { ...result, isLoading: action.payload };
 
+    case 'setDiscount':
+      return { ...result, discountAmount: action.payload };
+
+    case 'setTotalPrice':
+      return { ...result, totalPrice: action.payload };
+
+    case 'setAddressToDeliver':
+      return { ...result, addressToDeliver: action.payload };
+
+    case 'setOrderHistory':
+      return {
+        ...result,
+        orderHistory: [...result.orderHistory, action.payload],
+      };
+
     default:
       return result;
   }
@@ -38,5 +49,9 @@ export const dataInitialState = {
   wishList: [],
   addressList: [],
   userDetail: {},
-  isLoading: false,
+  isLoading: true,
+  discountAmount: 0,
+  totalPrice: 0,
+  addressToDeliver: {},
+  orderHistory: [],
 };
