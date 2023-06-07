c40d6df5f586c1358a4719cc09c1659c71b5dbd6
Vijayalakshmi G (she/her) <104077094+VijayalakshmiGanesh@users.noreply.github.com>
2023-06-07 09:22:48 +0530

    Added show password

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,12 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { NavLink } from 'react-router-dom';
+import { BiShow, BiHide } from 'react-icons/bi';
 
 function Login() {
   const [emailEntered, setEmailEntered] = useState('');
   const [passwordEntered, setPasswordEntered] = useState('');
-
+  const [showPassword, setShowPassword] = useState(false);
   const { LoginHandler, hasErrorOccured } = useContext(AuthContext);
 
   const TestCredshandler = () => {
@@ -14,56 +15,66 @@ function Login() {
   };
   return (
     <>
-      <div className='relative flex justify-center items-center  '>
+      <div className="relative flex justify-center items-center  ">
         {/* <div className='w-[400px] h-[400px] p-5 bg-white shadow-lg h-full flex flex-col items-center border-1 border-solid border-slate-700'> */}
         <div className=" my-16 py-5 px-7 drop-shadow-lg w-[370px] border-2 border-solid border-gray-50 shadow-lg">
-        <p className="text-2xl font-semibold text-center">Login</p>
-        <p className="text-gray-400 text-xs">
-          Please login using account detail below
-        </p>
-        <div className="my-5 max-w-xs">
-          {hasErrorOccured && (
-            <p className="px-3 py-1 bg-red-300 text-red-800 rounded border-solid border-red-800 w-full">
-              Incorrect login Credentials. Please try again.{' '}
-            </p>
-          )}
-          <label className='flex flex-col my-5 w-full'>
-            <span className='text-xs text-left'>Enter email</span>
-            <input
-            type="email"
-            placeholder="Enter email"
-            className="text-left w-full border-gray-500 px-4 py-3 border-[1px] border-solid border-stone-300 "
-            onChange={e => setEmailEntered(e.target.value)}
-            value={emailEntered}
-          />
-          </label>
-          <label className='flex flex-col mb-5'>
-            <span className='text-xs text-left'>Enter password</span>
-            <input
-            type="password"
-            placeholder="Enter password"
-            className=" text-left w-full border-gray-500 px-4 py-3 focus-visible:border-gray-800"
-            onChange={e => setPasswordEntered(e.target.value)}
-            value={passwordEntered}
-          />
-          </label>
-          <div className='flex mb-5 '>
-            <button
-            className="w-full text-white bg-pink-700 mx-1 p-3 rounded-md font-bold"
-            onClick={() => LoginHandler(emailEntered, passwordEntered)}
-          >
-            Sign in
-          </button>
-          <button
-            className="w-full  text-white bg-pink-700 mx-1 p-3 rounded-md font-bold"
-            onClick={() => TestCredshandler()}
-          >
-            Test Credentials
-          </button>
+          <p className="text-2xl font-semibold text-center">Login</p>
+          <p className="text-gray-400 text-xs">
+            Please login using account detail below
+          </p>
+          <div className="my-5 max-w-xs">
+            {hasErrorOccured && (
+              <p className="px-3 py-1 bg-red-300 text-red-800 rounded border-solid border-red-800 w-full">
+                Incorrect login Credentials. Please try again.{' '}
+              </p>
+            )}
+            <label className="flex flex-col my-5 w-full">
+              <span className="text-xs text-left">Enter email</span>
+              <input
+                type="email"
+                placeholder="Enter email"
+                className="text-left w-full border-gray-500 px-4 py-3 border-[1px] border-solid border-stone-300 "
+                onChange={e => setEmailEntered(e.target.value)}
+                value={emailEntered}
+              />
+            </label>
+            <label className="flex flex-col mb-5">
+              <span className="text-xs text-left">Enter password</span>
+              <div className="relative flex items-center ">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Enter password"
+                  className=" text-left w-full border-gray-500 px-4 py-3 focus-visible:border-gray-800"
+                  onChange={e => setPasswordEntered(e.target.value)}
+                  value={passwordEntered}
+                />
+                <button
+                  className="absolute right-0 p-2 text-lg"
+                  onClick={() => setShowPassword(prev => !prev)}
+                >
+                  {showPassword ? <BiHide /> : <BiShow className="" />}
+                </button>
+              </div>
+            </label>
+            <div className="flex mb-5 ">
+              <button
+                className="w-full text-white bg-pink-700 mx-1 p-3 rounded-md font-bold"
+                onClick={() => LoginHandler(emailEntered, passwordEntered)}
+              >
+                Sign in
+              </button>
+              <button
+                className="w-full  text-white bg-pink-700 mx-1 p-3 rounded-md font-bold"
+                onClick={() => TestCredshandler()}
+              >
+                Test Credentials
+              </button>
+            </div>
+            <NavLink to="/signup" className="text-pink-700 mx-1 p-3 font-bold ">
+              Don't have an account? Sign Up &gt;
+            </NavLink>
           </div>
-          <NavLink to="/signup" className="text-pink-700 mx-1 p-3 font-bold ">Don't have an account? Sign Up &gt;</NavLink>
         </div>
-      </div>
       </div>
     </>
   );
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,61 +1,86 @@
 import { useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import { NavLink } from 'react-router-dom';
+import { BiShow, BiHide } from 'react-icons/bi';
 
 function SignUp() {
   const [emailEntered, setEmailEntered] = useState('');
-    const [passwordEntered, setPasswordEntered] = useState('');
-    const [firstName, setFirstName] = useState("")
-    const [lastName, setLastName] = useState("");
+  const [passwordEntered, setPasswordEntered] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
 
   const { SignUpHander } = useContext(AuthContext);
 
-  
   return (
     <>
-      <div className="flex justify-center items-center flex-col h-full my-16 p-5 drop-shadow-lg border-1 border-solid">
-        <p className="text-2xl font-semibold">Sign Up</p>
-        <p className="text-gray-400 text-xs">
-          Please signup by entering the details below
-        </p>
-        
-        <div className="my-5 max-w-xs">
+      <div className="flex justify-center items-center flex-col ">
+        <div className=" my-16 py-5 px-7 drop-shadow-lg w-[370px] border-2 border-solid border-gray-50 shadow-lg">
+          <p className="text-2xl font-semibold">Sign Up</p>
+          <p className="text-gray-400 text-xs">
+            Please signup by entering the details below
+          </p>
+
+          <div className="my-5 max-w-xs">
             <div className="flex flex-row">
-                <label className='flex flex-col'>
-                    <span className='text-xs text-left' >Enter Firstname</span>
-                    <input type='text' placeholder='Adarsh' className="text-left w-full border-gray-500 px-4 py-3" onChange={(e) => setFirstName(e.target.value)}/>  
-                </label>
-                <label className='flex flex-col'>
-                    <span  className='text-xs text-left'>Enter Lastname</span>
-                    <input type='text' placeholder='Balika' className=" mx-1 text-left w-full border-gray-500 px-4 py-3" onChange={(e) => setLastName(e.target.value)} />  
-               </label>
+              <label className="flex flex-col">
+                <span className="text-xs text-left">Enter Firstname</span>
+                <input
+                  type="text"
+                  placeholder="Adarsh"
+                  className="text-left w-full border-gray-500 px-4 py-3"
+                  onChange={e => setFirstName(e.target.value)}
+                />
+              </label>
+              <label className="flex flex-col">
+                <span className="text-xs text-left">Enter Lastname</span>
+                <input
+                  type="text"
+                  placeholder="Balika"
+                  className=" mx-1 text-left w-full border-gray-500 px-4 py-3"
+                  onChange={e => setLastName(e.target.value)}
+                />
+              </label>
+            </div>
+            <label className="flex flex-col my-5">
+              <span className="text-xs text-left">Enter Email</span>
+              <input
+                type="email"
+                placeholder="test@gmail.com"
+                className=" text-left w-full border-gray-500 px-4 py-3"
+                onChange={e => setEmailEntered(e.target.value)}
+              />
+            </label>
+            <label className="flex flex-col my-5 my-5 ">
+              <span className="text-xs text-left">Enter Password</span>
+              <div className="relative flex items-center">
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="********"
+                  className="text-left w-full border-gray-500 px-4 py-3 focus-visible:border-gray-800"
+                  onChange={e => setPasswordEntered(e.target.value)}
+                />
+                <button
+                  className="absolute right-0 p-2 text-lg"
+                  onClick={() => setShowPassword(prev => !prev)}
+                >
+                  {showPassword ? <BiHide /> : <BiShow className="" />}
+                </button>
+              </div>
+            </label>
+
+            <button
+              className="mb-5 text-white bg-pink-700 mx-1 p-3 rounded-md w-full font-bold"
+              onClick={() =>
+                SignUpHander(emailEntered, passwordEntered, firstName, lastName)
+              }
+            >
+              Create account
+            </button>
+            <NavLink to="/login" className="text-pink-700 mx-1 p-3 font-bold ">
+              Already have an account &gt;
+            </NavLink>
           </div>
-                  <label className='flex flex-col my-5'>
-                      <span className='text-xs text-left' >Enter Email</span>
-                      <input
-            type="email"
-            placeholder="test@gmail.com"
-            className=" text-left w-full border-gray-500 px-4 py-3"
-           onChange={(e) => setEmailEntered(e.target.value)}
-          />
-                  </label>
-                  <label className='flex flex-col my-5 my-5 '>
-                      <span className='text-xs text-left' >Enter Password</span>
-                      <input
-            type="text"
-            placeholder="********"
-            className="text-left w-full border-gray-500 px-4 py-3 focus-visible:border-gray-800"
-           onChange={(e) => setPasswordEntered(e.target.value)}
-          />
-          </label>
-          
-          <button
-            className="mb-5 text-white bg-pink-700 mx-1 p-3 rounded-md w-full font-bold"
-            onClick={() => SignUpHander(emailEntered, passwordEntered, firstName, lastName)}
-          >
-            Create account
-                  </button>
-                  <NavLink to="/login" className="text-pink-700 mx-1 p-3 font-bold ">Already have an account &gt;</NavLink>
         </div>
       </div>
     </>
